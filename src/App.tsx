f47df920bf2743a5b09aaa8d6876d4ac7b500bd8
piyush15k <piyushkfeb@gmail.com>
2026-07-25 21:45:19 +0530

    Stage 1: project setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="min-h-screen bg-slate-50 flex items-center justify-center">
+      <h1 className="text-3xl text-red-500 font-bold">
+        Hello, World!
+      </h1>
+    </div>
+  )
+}
+
+export default App
